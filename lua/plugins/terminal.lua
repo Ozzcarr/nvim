ab0b900e09e226c2c1e9878cdedb8bf504e3d30b
Oscar Andersson <anderssonoscar03@gmail.com>
2025-01-28 21:09:51 +0100

    Added toggleterm as terminal

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,10 @@
+return {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+    opts = {
+        open_mapping = [[<C-n>]],
+        direction = "float",
+        persist_mode = true,
+    },
+}
