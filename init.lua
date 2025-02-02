fa5d2cb42da5a93950fb476c3f1add23d9ee3131
Oscar Andersson <anderssonoscar03@gmail.com>
2025-02-02 19:22:58 +0100

    Remove shada on startup and no vscode

--- init.lua
+++ init.lua
@@ -1,19 +1,5 @@
-if vim.g.vscode then
-    -- Map <Space> to show Which Key in normal and visual modes
-    vim.api.nvim_set_keymap(
-        "n",
-        "<Space>",
-        ":call VSCodeNotify('whichkey.show')<CR>",
-        { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-        "x",
-        "<Space>",
-        ":call VSCodeNotify('whichkey.show')<CR>",
-        { noremap = true, silent = true }
-    )
-else
-    require("config.keymaps")
-    require("config.lazy")
-    require("config.options")
-end
+vim.o.shada = ""
+
+require("config.keymaps")
+require("config.lazy")
+require("config.options")
