cf01863f9f7986beb1ee7761b90746bc9b173557
Oscar Andersson <anderssonoscar03@gmail.com>
2025-02-09 13:34:44 +0100

    Zen mode width now working as intended

--- lua/plugins/zen.lua
+++ lua/plugins/zen.lua
@@ -1,9 +1,12 @@
 return {
     "folke/zen-mode.nvim",
     opts = {
-        width = 120,
+        window = {
+            width = 130,
+        },
     },
-    config = function()
+    config = function(_, opts)
+        require("zen-mode").setup(opts)
         vim.keymap.set("n", "<leader>z", ":ZenMode<cr>", { desc = "Open Zen Mode" })
-    end
+    end,
 }
